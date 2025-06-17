6c41d4b5d6bfcbb071c099122f8d2fa21c28db65
Andreacapu <capuani.andrea@gmail.com>
2025-06-17 17:29:33 +0200

    Ripasso

---
+++ Recupero/Esercizio1.py
@@ -0,0 +1,13 @@
+class Frazione:
+    def __init__(self, setter: float, getter: float):
+        self.setter = setter
+        self.getter = getter
+
+    def setter(self) -> None:
+        if 
+
+    def __str__(self, setter:float, getter: float) -> str:
+        return setter, getter
+    
+
+        
