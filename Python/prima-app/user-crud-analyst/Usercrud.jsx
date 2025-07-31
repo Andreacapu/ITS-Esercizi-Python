5a2d30bd744b779bc5091f71b315e846afe6413f
Andreacapu <capuani.andrea@gmail.com>
2025-07-31 17:07:50 +0200

    cifrario e jsx

---
+++ Python/prima-app/user-crud-analyst/Usercrud.jsx
@@ -0,0 +1,7 @@
+const UserCrud =() =>{
+    return (<div>
+        <h1>USER <CRUD2></CRUD2></h1>
+    </div>)
+}
+
+export default UserCrud
---
+++ Python/randombit.py
@@ -0,0 +1,31 @@
+import sys
+import random
+
+for s in sys.argv:
+    print(s)
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <file name>")
+
+#1)devo leggere tutto il file
+data = None
+with open(sys.argv[1], "rb") as f:
+    data = f.read()
+
+#2)devo scegliere un byte casuale del file
+pos = random.randint(0, len(data)-1)
+
+#3)prendo un bit casuale del byte
+bit = random.randint(0, 7)
+
+#4)devo cambiare il valore del bit <bit> di data[pos] (uso lo xor)
+#in sostanza devo costruire un byte di tutti 0 e un solo 1 in un posto giusto
+byte = data[pos]
+byte = byte ^ (1<< bit) #ho rovescieto il bit bit-esimo del byte
+
+data = data[:pos] + bytes([byte]) + data[pos+1:]
+
+with open(sys.argv[1], "wb") as f:
+    f.write(data)
+
+print(f"Modificato il bit {bit} al posto {pos} nel file {sys.argv[1]}")
